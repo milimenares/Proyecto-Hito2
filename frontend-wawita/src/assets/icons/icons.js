4648033a14c5cac43a986e29e82c02f230bf5b87
Melany Menares <menares.melany@gmail.com>
2025-08-10 00:28:54 -0400

    feat: agregar icons fontawesome

---
+++ frontend-wawita/src/assets/icons/icons.js
@@ -0,0 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHeart as faHeartSolid, faHeart, faCircleUser, faBagShopping, faPlus, faMinus, faXmark, faPencil } from "@fortawesome/free-solid-svg-icons"
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
+import { faFacebook, faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+
+export {
+    FontAwesomeIcon,
+    faFacebook,
+    faInstagram,
+    faWhatsapp,
+    faBagShopping,
+    faCircleUser,
+    faHeart,
+    faHeartSolid,
+    faHeartRegular,
+    faPlus,
+    faMinus,
+    faXmark,
+    faPencil
+}
